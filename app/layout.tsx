66aa8c4fa7abe11f7f04b4d80d00c310411f457c
Chirayu Arya <chirayuarya@Chirayus-MacBook-Pro-M3-Max.local>
2026-04-10 23:11:35 -0400

    Remove custom cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import CustomCursor from "./components/CustomCursor";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,10 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
-      <body>
-        <CustomCursor />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
